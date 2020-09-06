d3f9f2a006a3c976a2c7d5c26ce8648e424b84b2
andiwoedwin <andiwoedwin@gmail.com>
2020-09-06 07:18:12 +0300

    add accounts.py file

---
+++ accounts.py
@@ -0,0 +1,29 @@
+
+
+class Accounts:
+    def __init__(self, first_name, last_name, user_name, password):
+
+        '''
+        Methods to define the properties for each user object will hold.
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+#     user_accounts = []
+
+#     def save_account(self):
+#         Accounts.user_accounts.append(self)
+    
+#     def delete_account(self):
+#         Accounts.user_accounts.remove(self)
+
+
+# class Password:
+#     '''
+#     Class to create account passwords and save their information.
+#     '''
+
+#     passwords_list
