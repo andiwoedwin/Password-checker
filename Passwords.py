cedfd40b4e8b20506be7a86f02d1758a156148e8
andiwoedwin <andiwoedwin@gmail.com>
2020-09-06 17:53:43 +0300

    add password.py file

---
+++ Passwords.py
@@ -0,0 +1,17 @@
+from accounts import Accounts
+def create_accounts_credential(first_name,last_name, user_name, password):
+    new accounts = Accounts(first_name, last_name, user_name,password)
+    return new_account_name
+
+def save_account_credential(account):
+    account.save_account()
+
+def main():
+    print('Hey enter your first name')
+    first_name = input()
+    print('Enter your last name')
+    last_name = input()
+    print('Enter your user name')
+    user_name = input()
+    password
+
