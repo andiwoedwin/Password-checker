78335a8df97af86e8f2a25795b80600f2c20a081
andiwoedwin <andiwoedwin@gmail.com>
2020-09-07 11:47:05 +0300

    add if statement in password.py

--- Passwords.py
+++ Passwords.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.8
 from account import Account
 
-def create_account(fname,lname,phone,email):
+def create_account(fname,lname,phone,password):
     '''
     Function to create. new account
     '''
-    new_account = Account(fname,lname,phone,email)
+    new_account = Account(fname,lname,phone,password)
     return new_account
 
 def save_accounts(account):
@@ -42,7 +42,7 @@ def display_accounts():
 def main():
     print("Hello! Welcome to your account list. What is your name?")
     user_name = input()
-    print(f"Hello {user_name}. what would you like to do?")
+    print("Hello "+ user_name + " what would you like to do?")
     print('\n')
     while True:
                     print("Follow the following short codes : cna- create a new account, da - display accounts, fa -find a account, ex -exit the account ")
@@ -54,40 +54,40 @@ def main():
                             f_name = input()
                             print("Last name ...")
                             l_name = input()
-                            print("Phone number ...")
+                            print("user account ...")
                             p_number = input()
-                            print("Email address ...")
+                            print("password ...")
                             e_address = input()
                             save_accounts(create_account(f_name,l_name,p_number,e_address)) # create and save new account.
                             print ('\n')
-                            print(f"{f_name} {l_name} your account has been created")
+                            print(f_name + l_name + "your account has been created")
                             print ('\n')
                     elif short_code == 'da':
                             if display_accounts():
                                     print("Here is a list of all your accounts")
                                     print('\n')
                                     for account in display_accounts():
-                                        print(f"Name: {account.first_name} {account.last_name}")
-                                        print(f"Phone number: {contact.phone_number}")
-                                    print(f"Email address: {account.email}")
+                                        print("Name: account.first_name + account.last_name")
+                                        print("user account: contact.user_account")
+                                    print("password : account.password")
                                     print('\n')
                             else:
                                     print('\n')
                                     print("You dont seem to have any accounts saved yet")
                                     print('\n')
                     elif short_code == 'fa':
-                            print("Enter the number you want to search for")
-                            search_number = input()
-                            if check_existing_accounts(search_number):
-                                    search_account = find_account(search_number)
-                                    print(f"{search_account.first_name} {search_account.last_name}")
+                            print("Enter the account you want to search for")
+                            search_account = input()
+                            if check_existing_accounts(search_account):
+                                    search_account = find_account(search_account)
+                                    print("search_account.first_name + search_account.last_name")
                                     print('-' * 20)
-                                    print(f"Phone number.......{search_account.phone_number}")
-                                    print(f"Email address.......{search_account.email}")
+                                    print("user account....... search_account.user_account")
+                                    print("password.......search_account.password")
                             else:
                                     print("That account does not exist")
                     elif short_code == "ex":
-                            print("Bye .......")
+                            print("See you then .......")
                             break
                     else:
                             print("I really didn't get that. Please use the short codes")
